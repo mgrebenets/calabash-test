63006f08dcd1b2957888033b170f518de2ede938
Maksym Grebenets <maksym.grebenets@news.com.au>
2013-05-28 11:47:32 +1000

    Initial Commit

---
+++ calabash-test/NLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NLCAppDelegate.h
+//  calabash-test
+//
+//  Created by Grebenets, Maksym on 5/28/13.
+//  Copyright (c) 2013 News Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ calabash-test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  calabash-test
+//
+//  Created by Grebenets, Maksym on 5/28/13.
+//  Copyright (c) 2013 News Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NLCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NLCAppDelegate class]));
+    }
+}
